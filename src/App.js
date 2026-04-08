256a6c9c1532776539ad6bb31db96d57ba9f8527
saniasadaf <samsadaf764@gmail.com>
2026-04-08 14:37:44 +0530

    trigger redeploy

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
-function App() {
+function App()   {
   return (
     <div>
       <Navbar />
